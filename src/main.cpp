098b3aea5728f56737ae5e132455828f17df09e4
Peter <hewmorist@gmail.com>
2024-12-31 21:02:43 +0000

    Changed main to test LED GPIO pin

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,18 @@
 #include <stdio.h>
 #include "driver/gpio.h"
 
-// The XIAO ESP32C6 has a built-in LED
+// The XIAO ESP32C6 has a built-in LED on GPIO15 active low.
+
+#define ONBOARD_LED 15
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output
-  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize digital pin ONBOARD_LED as an output
+  pinMode(ONBOARD_LED, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED_BUILTIN on 
+  digitalWrite(ONBOARD_LED, LOW);   // turn the ONBOARD_LED on 
   delay(1000);               // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED_BUILTIN off
+  digitalWrite(ONBOARD_LED, HIGH);    // turn the ONBOARD_LED off
   delay(1000);               // wait for a second
 }
